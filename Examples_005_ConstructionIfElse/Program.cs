5743c2b4eadeda7909224bf476b37a4e3a5e4ba5
Sashok <sergeevbass@gmail.com>
2022-10-12 04:52:52 +0300

    Examples 5

---
+++ Examples_005_ConstructionIfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя ");
+String username = Console.ReadLine();
+
+if (username.ToLower() == "Маша") {
+    Console.WriteLine ("Ура, это же Маша!!!!!");
+}
+else {
+    Console.Write("Привет, " + username);
+}
