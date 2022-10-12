869e116372e626d7f774ff65223bf27d4d92bca9
Sashok <sergeevbass@gmail.com>
2022-10-12 12:56:46 +0300

    Resolve task with two friends

---
+++ TwoDogs/Program.cs
@@ -0,0 +1,22 @@
+﻿int distance = 10000, 
+firstSpeed = 1, 
+secondSpeed = 2, 
+dogSpeed = 5,
+friend = 2,
+time = 0,
+count = 0;
+
+while (distance > 10) {
+
+    if (friend == 1) {
+        time = distance  / (firstSpeed + dogSpeed);
+        friend = 2;
+    }
+    else {
+        time = distance / (secondSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstSpeed + secondSpeed) * time;
+    count++;
+}
+Console.WriteLine("Собака пробежит: " + count); 
